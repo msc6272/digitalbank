ab619f053473920d77a723d62bba5f92cb08ca88
Marcello Cruz <marcellocruz.aluno@unipampa.edu.br>
2022-11-20 22:00:40 -0300

    Final

--- src/main/java/dio/quebec/digitalbank/service/Utilities.java
+++ src/main/java/dio/quebec/digitalbank/service/Utilities.java
@@ -1,5 +1,16 @@
 package dio.quebec.digitalbank.service;
 
+/*
+ * Links com recursos informando sobre como implementar os métodos abaixo
+ * @see <a href="https://www.cpfcnpj.com.br/dev/">Documentação API - CPF.CNPJ</a>
+ * @see <a href="http://www.cadcobol.com.br/calcula_cpf_cnpj_caepf.htm">Cálculo digitos de verificação</a>
+ * @see <a href="https://www.cjdinfo.com.br/publicacao-calculo-digito-verificador">Cálculo de Dígito Verificador</a>
+ * @see <a href="https://www.cjdinfo.com.br/utilitario-calculo-digito-modulo-11">Cálculo Dígito Módulo 11</a>
+ * @see <a href="https://www.loja.serpro.gov.br/consultacpf">Procurando automação de acesso a informações atualizadas do Cadastro de Pessoas Físicas (CPF)?</a>
+ * @see <a href="https://apicenter.estaleiro.serpro.gov.br/documentacao/consulta-cpf/pt/quick_start/">Como autenticar na API Consulta CPF</a>
+ * 
+ * Infelizmente o tempo foi curto para implementar tudo que eu gostaria.
+ */
 public final class Utilities {
     public static int calculaDvMod11(int identificador) {
         //Implementar o cálculo do DV pelo algoritmo MOD11
